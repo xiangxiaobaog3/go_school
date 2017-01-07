bc5995e93d7f4c6f580b4c733bd9387b78703415
xiangxiaobao <943300866@qq.com>
2017-01-07 13:37:55 +0800

    添加结构体学习

---
+++ struct_file.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+
+//声明一个新的类型
+type person struct {
+	name string
+	age int
+}
+
+// 比较两个人的年龄，返回年龄大的那个人，并且返回年龄差
+// struct也是传值的
+
+func Older(p1, p2 person) (person, int) {
+	if p1.age > p2.age {	// 比较p1和p2这两个人的年龄
+		return p1, p1.age - p2.age
+	}
+	return p2, p2.age - p1.age
+}
+
+func main() {
+	var tom person
+
+	// 赋值初始化
+	tom.name, tom.age = "Tom", 18
+
+	// 两个字段都写清楚的初始化
+	bob := person{age:25, name:"Bob"}
+
+	// 按照struct定义顺序初始化值
+	//paul := person{"Paul", 43}
+
+	td_Older, tb_diff := Older(tom, bob)
+
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		tom.name, bob.name, td_Older.name, tb_diff)
+}
+
