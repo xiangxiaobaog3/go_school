8181c0bd503cb3ed40a34a23e1caf5644276c24b
xiangxiaobao <943300866@qq.com>
2017-01-07 19:13:08 +0800

    添加结构体学习

--- ipconfig/ip_info.go
+++ ipconfig/ip_info.go
@@ -18,6 +18,7 @@ func main() {
 	check(err)
 	// _（下划线）是个特殊的变量名，任何赋予它的值都会被丢弃
 	// 由于 Go 支持 “多值返回”, 而对于“声明而未被调用”的变量, 编译器会报错, 在这种情况下, 可以使用_来丢弃不需要的返回值
+	// _ 循环的是索引所以忽略
 	for _, a := range addrs {
 		fmt.Printf("Interface %q, address %v\n", ifi.Name, a)
 	}
